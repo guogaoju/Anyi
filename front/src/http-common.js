4ff7e9a078f8bc8592fc72a67f6275d8c31892cd
guogaoju <2248147607@qq.com>
2021-12-30 13:53:23 +0800

    安义

---
+++ front/src/http-common.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default axios.create({
+  //local
+  baseURL: "http://localhost:8088/api",
+  //server
+  // baseURL: "http://106.15.5.29:8088/api",
+
+  headers: {
+    "Content-type": "application/json"
+  }
+});
